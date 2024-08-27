525aebc61e72a8dfbe1fcc9bc855c24e6843a823
JuanSauri <juan.sauri.developer@gmail.com>
2024-08-27 02:32:01 -0600

    primera parte del curso

---
+++ curso-python.py
@@ -0,0 +1,43 @@
+print("Este es el primer hola mundo!, aqui empieza el curso, comencemos!")
+
+#Tipos de datos:
+
+#"string"
+#'string'
+
+#"""tus datos son:
+#     nombre: juan 
+#     apellido: sauri"""
+
+#'''tus datos son:
+#     nombre: juan 
+#     apellido: sauri'''
+
+# los string son cadenas de texto y se pueden definir con las comillas dobles o simples
+
+# Numeros:
+# Numeros enteros ej. 40
+# Punto flotante  ej. 40.5
+
+# Booleano
+# los datos booleanos son True y False, con mayuscula al principio por que python es case sensitive, se utilizan con operadores booleanos por ejemplo
+
+#Variables:
+# las variables son para almacenar informacion util en forma de datos
+# las variables se declaran
+# las variables se definen
+
+nombre = "Juan Sauri"
+print(nombre)
+numero1 = 20
+numero2 = 30
+
+print(numero1 + numero2)
+
+# concatenar
+edad = " 37 años de edad"
+
+informacion = nombre + edad
+print(informacion)
+
+
